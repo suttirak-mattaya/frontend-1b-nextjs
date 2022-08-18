2b6016ca67c88a512f95fee2b16f299bde64bfdf
Suttirak Mattayawerakiat <6210110383@psu.ac.th>
2022-08-18 22:47:19 +0700

    edit node.js.yml

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,8 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-const isProd = process.env.NODE_ENV === 'production'
-module.exports = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? '/frontend-1b-nextjs/' : '',
-}
